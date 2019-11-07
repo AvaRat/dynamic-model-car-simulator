b7e02f95e62c085ca91e15c21321c47add1af4bb
Marcel Kalinski <marcelos.kalinski@gmail.com>
2019-11-08 00:35:49 +0100

    working on huge initial values

--- src/car_model.cpp
+++ src/car_model.cpp
@@ -1,18 +1,17 @@
 #include"../include/car_model.h"
 
-// test gita!xxxxxxxxxxxxxxxxxxxxxx
 
 Model::Model(double Dt, double initial_speed)
 {
 	//zmienne stanu dla t = 0
-    yaw_angle = 0;
+	yaw_angle = 0;
 	yaw_rate = 0;
 	lat_vel = 0;
 	long_vel = initial_speed;
 	long_pos = 0;
 	lat_pos = 0;
 	//parametry wpisane na razie na pale
-    dT = Dt;
+	dT = Dt;
 	r = 0.3;
 	b = 2;
 	a = 2;
@@ -27,69 +26,72 @@ Model::Model(double Dt, double initial_speed)
 
 
  //   visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "/path";
-    marker.header.stamp = ros::Time::now();
+	// Set the frame ID and timestamp.  See the TF tutorials for information on these.
+	marker.header.frame_id = "/path";
+	marker.header.stamp = ros::Time::now();
 
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "path";
-    marker.id = 0;
+	// Set the namespace and id for this marker.  This serves to create a unique ID
+	// Any marker sent with the same namespace and id will overwrite the old one
+	marker.ns = "path";
+	marker.id = 0;
 
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = 0;
+	// Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+	marker.type = 0;
 
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
+	// Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+	marker.action = visualization_msgs::Marker::ADD;
+	// Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+	marker.pose.position.x = 0;
+	marker.pose.position.y = 0;
+	marker.pose.position.z = 0;
+	marker.pose.orientation.x = 0.0;
+	marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0;
+	marker.pose.orientation.w = 1.0;
 
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 0.6;
-    marker.scale.y = 0.15;
-    marker.scale.z = 0.1;
+	// Set the scale of the marker -- 1x1x1 here means 1m on a side
+	marker.scale.x = 0.6;
+	marker.scale.y = 0.15;
+	marker.scale.z = 0.1;
 
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
+	// Set the color -- be sure to set alpha to something non-zero!
+	marker.color.r = 0.0f;
+	marker.color.g = 1.0f;
+	marker.color.b = 0.0f;
+	marker.color.a = 1.0;
 
-    marker.lifetime = ros::Duration(5);
+	marker.lifetime = ros::Duration(5);
+
+	//first command to initialize all values
+	command(0,0);
 }
 
 void Model::command(double torque , double steering_angle)
 {
-    /*
-    std::cout << "command called\n give new coordinates";
-    std::cout << "\nx_pos: \t";
-    std::cin >> long_pos;
-    std::cout << "\ny_pos: \t";
-    std::cin >> lat_pos;
-    std::cout << "\nangle: \t";
-    std::cin >> yaw_angle;
-    std::cout << std::endl;
-    */
+	/*
+	std::cout << "command called\n give new coordinates";
+	std::cout << "\nx_pos: \t";
+	std::cin >> long_pos;
+	std::cout << "\ny_pos: \t";
+	std::cin >> lat_pos;
+	std::cout << "\nangle: \t";
+	std::cin >> yaw_angle;
+	std::cout << std::endl;
+	*/
 		//przeliczenie zadanego momentu na sile wzdluzna kola
 		long_for_f = torque / r;
 		long_for_r = long_for_f; //zakladam rowne rozlozenie momentu na os przod/tyl
 
-        if(fabs(long_vel) < 0.0001)
+		if(fabs(long_vel) < 0.0001)
 		{
-            slip_angle_f = 0;
-		    slip_angle_r = 0;
-            wrong_slip++;
-        }else
-        {
-            slip_angle_f = steering_angle - ((a * yaw_rate + lat_vel) / long_vel);
-		    slip_angle_r = (b * yaw_rate - lat_vel) / long_vel;
-        }
+			slip_angle_f = 0;
+			slip_angle_r = 0;
+			wrong_slip++;
+		}else
+		{
+			slip_angle_f = steering_angle - ((a * yaw_rate + lat_vel) / long_vel);
+			slip_angle_r = (b * yaw_rate - lat_vel) / long_vel;
+		}
 
 
 
@@ -119,54 +121,54 @@ void Model::command(double torque , double steering_angle)
 
 std::vector<double> Model::get_position()
 {
-    std::vector<double> vec = {long_pos, lat_pos, yaw_angle};
-    return vec;
+	std::vector<double> vec = {long_pos, lat_pos, yaw_angle};
+	return vec;
 }
 void Model::get_data(std::map<std::string, double> &data)
 {
-    data["x"] = long_pos;
-    data["y"] = lat_pos;
-    data["lat_vel"] = lat_vel;
-    data["long_vel"] = long_vel;
-    data["yaw_angle"] = yaw_angle;
-    data["yaw_rate"] = yaw_rate;
-    data["slip_angle_f"] = slip_angle_f;
-    data["slip_angle_r"] = slip_angle_r;
-    data["norm_load_f"] = norm_load_f;
-    data["norm_load_r"] = norm_load_r;
-    data["slip_angle_est_f"] = slip_angle_est_f;
-    data["slip_angle_est_r"] = slip_angle_est_r;
-    data["lat_for_f"] = lat_for_f;
-    data["lat_for_r"] = lat_for_r;
+	data["x"] = long_pos;
+	data["y"] = lat_pos;
+	data["lat_vel"] = lat_vel;
+	data["long_vel"] = long_vel;
+	data["yaw_angle"] = yaw_angle;
+	data["yaw_rate"] = yaw_rate;
+	data["slip_angle_f"] = slip_angle_f;
+	data["slip_angle_r"] = slip_angle_r;
+	data["norm_load_f"] = norm_load_f;
+	data["norm_load_r"] = norm_load_r;
+	data["slip_angle_est_f"] = slip_angle_est_f;
+	data["slip_angle_est_r"] = slip_angle_est_r;
+	data["lat_for_f"] = lat_for_f;
+	data["lat_for_r"] = lat_for_r;
 }
 
 double Model::get_max_torque()
 {
-    return max_torque;
+	return max_torque;
 }
 
 void Model::publish_pose(ros::Publisher *pub)
 {
-    marker.pose.position.x = long_pos;
-    marker.pose.position.y = lat_pos;
-    double w, x, y, z;
-    double cy = cos(yaw_angle * 0.5);
-    double sy = sin(yaw_angle * 0.5);
-    double cp = cos(0 * 0.5);
-    double sp = sin(0 * 0.5);
-    double cr = cos(0 * 0.5);
-    double sr = sin(0 * 0.5);
+	marker.pose.position.x = long_pos;
+	marker.pose.position.y = lat_pos;
+	double w, x, y, z;
+	double cy = cos(yaw_angle * 0.5);
+	double sy = sin(yaw_angle * 0.5);
+	double cp = cos(0 * 0.5);
+	double sp = sin(0 * 0.5);
+	double cr = cos(0 * 0.5);
+	double sr = sin(0 * 0.5);
 
  //   Quaternion q;
-    w = cy * cp * cr + sy * sp * sr;
-    x = cy * cp * sr - sy * sp * cr;
-    y = sy * cp * sr + cy * sp * cr;
-    z = sy * cp * cr - cy * sp * sr;
-    marker.pose.orientation.x = x;
-    marker.pose.orientation.y = y;
-    marker.pose.orientation.z = z;
-    marker.pose.orientation.w = w;
+	w = cy * cp * cr + sy * sp * sr;
+	x = cy * cp * sr - sy * sp * cr;
+	y = sy * cp * sr + cy * sp * cr;
+	z = sy * cp * cr - cy * sp * sr;
+	marker.pose.orientation.x = x;
+	marker.pose.orientation.y = y;
+	marker.pose.orientation.z = z;
+	marker.pose.orientation.w = w;
 
-    pub->publish(marker);
+	pub->publish(marker);
 }
 
--- src/sim_main.cpp
+++ src/sim_main.cpp
@@ -4,33 +4,27 @@
 using namespace std;
 int main(int argc, char **argv)
 {
-
-  double dT = 0;
-  double initial_speed = 0;
-  ros::init(argc, argv, "simulator");
-  ros::NodeHandle n;
-  ros::Rate r(100);
-  n.param("dT", dT, 0.01);
-  n.param("initial_speed", initial_speed, 0.0);
-  ros::Publisher pose_pub = n.advertise<geometry_msgs::PoseStamped>("pose", 1);
-  ros::Publisher speed_pub = n.advertise<std_msgs::Float64>("speed", 1);
-  Simulator simulator(pose_pub, speed_pub, dT, initial_speed);
-
-  ros::Subscriber distance_sub = n.subscribe("s", 1, &Simulator::track_progress_callback, &simulator);
-  ros::Subscriber error_pub = n.subscribe("d", 1, &Simulator::error_callback, &simulator);
-  ros::Subscriber cmd_sub = n.subscribe("model_control", 10, &Simulator::cmd_callback, &simulator);
-  simulator.set_cmd_subscriber(cmd_sub);
-
-  ROS_INFO("started simulation with \ndT = %lf\ninitial_speed = %lf", dT, initial_speed);
-  char ch;
-
-  while(ros::ok())
-  {
-    simulator.next_time_step();
-    ros::spinOnce();
-    r.sleep();
-  }
-
-
-  return 0;
+	cout << "in mainXXXXX\n";
+	double dT = 0;
+	double initial_speed = 0;
+	ros::init(argc, argv, "simulator");
+	ros::NodeHandle n;
+	ros::Rate r(100);
+	n.param("dT", dT, 0.01);
+	n.param("initial_speed", initial_speed, 0.0);
+	ros::Publisher pose_pub = n.advertise<geometry_msgs::PoseStamped>("pose", 1);
+	ros::Publisher speed_pub = n.advertise<std_msgs::Float64>("speed", 1);
+	Simulator simulator(pose_pub, speed_pub, dT, initial_speed);  
+	ros::Subscriber distance_sub = n.subscribe("s", 1, &Simulator::track_progress_callback, &simulator);
+	ros::Subscriber error_pub = n.subscribe("d", 1, &Simulator::error_callback, &simulator);
+	ros::Subscriber cmd_sub = n.subscribe("model_control", 10, &Simulator::cmd_callback, &simulator);
+	simulator.set_cmd_subscriber(cmd_sub);  
+	ROS_INFO("started simulationXXXX with \ndT = %lf\ninitial_speed = %lf", dT, initial_speed);
+	while(ros::ok())
+	{
+	  simulator.next_time_step();
+	  ros::spinOnce();
+	  r.sleep();
+	} 
+	return 0;
 }
